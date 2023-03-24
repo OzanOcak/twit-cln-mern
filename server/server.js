bc1ba8fa7327b6c6971cf4745dfcc70fb23a94fa
OzanOcak <ozanocak@gmail.com>
2023-03-24 14:02:33 -0400

    db conn, user router and model

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoutes from "./routes/users.js";
+
 const app = express();
 const PORT = 8000;
+dotenv.config();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("connect to mongodb database");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, (req, res) => {
+  connect();
   `console.log(server is runninin on port ${PORT}`;
 });
